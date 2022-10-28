56b40c4f24cb074412c7b153a975e00cf96e2ebc
401 <401@academicos.cesde.edu.co>
2022-10-28 11:56:57 -0500

    cambios del proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.example.clases.Empleado;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // PARA UNA USAR UNA CLASE DEBO CREAR UN OBJETO
+        //DE ESA CLASE
+        // UN OBEJTO ES UNA VARIABLE QUE NO ES PRIMITIVA
+      Empleado objeto=new Empleado();
+
+
+
+
+       Empleado Objeto= new Empleado();
+
+       //USAR UNA CLASE ES ACCEDER  A SUS ATRIBUTOS Y METODOS
+        // ACCEDO A UN ATRIBUTO PARA ALAMACENAR UN VALOR EN MEMORIA
+
+        System.out.println(objeto);
+
+
+
+
+        //ACCEDO A UN METODO PARA DISFRUTAR LA FUNCIONALIDAD DEL SUBPROGRAMA
+
+
+    }
+}
---
+++ src/main/java/org/example/clases/Empleado.java
@@ -0,0 +1,53 @@
+package org.example.clases;
+
+public class Empleado {
+
+    //ATRIBUTOS
+//(VARIABLES DE JAVA)
+//(FUERTES TIPADAS)
+    public String id;
+    public String nombre;
+    String apellido;
+    String email;
+    String direccion;
+    String telefono;
+    String documento;
+    char tipoAfiliacionCajaCompensacion;
+    byte edad;
+    byte numeroHijos;
+    float salario;
+    byte numeroBenefeciario;
+    double cuotaFondoEmpleado;
+    double deduccionPrestamoVivienda;
+    double deduccionGimnasio;
+    double demandaAlimnentos;
+    double deduccionSalud;
+    double deduccionPensiones;
+    double deduccionCajaCompensacion;
+    double subsidioCajaCompensacion;
+    double auxilioTransporte;
+    double dotacion;
+    double auxilioEscolar;
+
+//CONSTRUCTOR
+//(METODO ESPECIAL)
+//(CREAR OBJETOS DE LA CLASE)
+
+    public Empleado() {
+
+    }
+
+//METODO DE CLASE
+    //(FUNCIONES DE JAVA)
+    //(TODA FUNCION RETORNA ALGO DE JAVA)
+    //CUANDO RETORNO NADA (VOID)
+
+    public void  saludar (){
+        System.out.println("hello world");
+    }
+
+
+
+
+
+}
